6a4bf4d1866b624fc44f288b9ea34cb4c6722152
wideem <wideem@gmail.com>
2023-03-25 23:45:49 +0200

    finished with add player

--- frontend/pages/add-player/scripts.js
+++ frontend/pages/add-player/scripts.js
@@ -1,21 +1,24 @@
-import { saveStudent } from "../../commons/requests.js";
+import { savePlayer } from "../../commons/requests.js";
 
 const handleFormSubmit = async () => {
-  const form = document.getElementById("addStudentForm").querySelector("form");
+  const form = document.getElementById("addPlayerForm").querySelector("form");
   form.addEventListener("submit", async (e) => {
     e.preventDefault();
-    const student = {
-      name: form.studentName.value,
-      lastname: form.studentLastname.value,
+    const player = {
+      name: form.playerName.value,
+      lastname: form.playerLastname.value,
+      email: form.playerEmail.value,
+      personCode: form.playerPersonCode.value,
+      startDate: form.playerStartDate.value,
     };
-    await saveStudent(student);
+    await savePlayer(player);
     window.location.reload();
   });
 };
 
 const handleCancelButton = () => {
   document.getElementById("cancelButton").addEventListener("click", () => {
-    window.location.replace("../student-list/student-list.html");
+    window.location.replace("../player-list/player-list.html");
   });
 };
 
